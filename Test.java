09e3634ada9a6254d4e4fafd3122829a208713ee
0511junpei <shok2gok.tdos3ky@icloud.com>
2023-10-21 02:03:13 +0900

    stash
    stash

--- Test.java
+++ Test.java
@@ -9,6 +9,6 @@ class Test {
     System.out.println("Hello2");
     System.out.println("Hello3");
     System.out.println("Hello4");
-    System.out.println("Hello5");
+    System.out.println("Hello5aaaa");
   }
 }
